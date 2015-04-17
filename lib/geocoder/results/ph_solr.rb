b8cba85b5b25945e0cc42bc909db340dd09edb04
ottimo <matteo.rosi@gmail.com>
2015-04-17 17:32:31 +0200

    convert data key from camel case in underscore inside lookup result

--- lib/geocoder/results/ph_solr.rb
+++ lib/geocoder/results/ph_solr.rb
@@ -2,5 +2,36 @@ require 'geocoder/results/google'
 
 module Geocoder::Result
   class PhSolr < Google
+
+    def initialize(data)
+      super(data)
+      recursive_snakecase_keys(@data)
+    end
+
+    def recursive_snakecase_keys(h)
+      case h
+      when Hash
+        Hash[
+          h.map do |k, v|
+            [ snakecase!(k), recursive_snakecase_keys(v) ]
+          end
+        ]
+      when Enumerable
+        h.map { |v| recursive_snakecase_keys(v) }
+      else
+        h
+      end
+    end
+
+    def snakecase!(str)
+      #gsub(/::/, '/').
+      str.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr('-', '_').
+        gsub(/\s/, '_').
+        gsub(/__+/, '_').
+        downcase
+    end
+
   end
 end
